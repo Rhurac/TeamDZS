c3fd1ba1b221340d36891080d096b8bd742a7014
Zachary <zcleary1@ksu.edu>
2016-04-12 13:01:07 -0500

    Landing, authentication, sign in form, sign up form added

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.disable('x-powered-by');
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
-app.use(favicon(__dirname + '/public/images/powercat.png'));
+app.use(favicon(__dirname + '/public/images/website_logo.png'));
 app.use(sessions({
   cookieName: 'session',
   secret: 'StarCraftKittens1986',
@@ -21,6 +21,7 @@ app.use(sessions({
 app.use(express.static(path.join(__dirname, "/client")));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.static(path.join(__dirname, "/views")));
+app.use(function(req, res, next) { res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0'); next(); });
 
 var index = require('./controllers/index');
 app.get('/login', index.landing);
@@ -28,11 +29,17 @@ app.get('/', index.landing);
 app.get('/home', collateFilteredQuestions, index.home);
 app.get('/about', index.about);
 app.get('/contact', index.contact);
-// app.get('/users', adminOnly, index.users);
 
 var sessions = require('./controllers/sessions');
 app.get('/sessions/new', sessions.new);
 app.post('/sessions/create', sessions.create);
+
+var users = require("./controllers/users");
+app.get("/users/index", users.index);
+app.get("/users/new", users.new);
+app.post("/users/create", users.create);
+app.get("/users/show", users.show);
+app.post("/users/:id/update", users.update);
 
 app.listen(app.get('port'), function(){
   console.log('Express started. Server listening on port 3000. Press Ctrl-C to terminate');
--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,74 @@
-var db = require('../db');
+var db = require('../db'),
+  formidable = require('formidable'),
+  encryption = require("../database/encryption"),
+  fs = require("fs");
+
 
 var users = {
+  index: function(req, res){
+    db.all("SELECT * FROM users", function(err, all){
+      res.render("users/index", {users: all});
+    });
+  },
 
+  new: function(req, res){
+    res.render("users/new", {layout:"landing"});
+  },
+
+  create: function(req, res){
+    var form = formidable.IncomingForm();
+    form.parse(req, function(err, fields, files){
+      if(err) return console.error(err);
+      var salt = encryption.salt();
+
+      db.serialize(function(){
+        db.get("SELECT * FROM users WHERE username = ?", fields.username, function(err, row){
+          if(err) return console.error("There was an error while processing your request.");
+          if(row){
+            return("username");
+          }
+        });
+
+        db.run("INSERT INTO users (fname, lname, picture, username, email, admin, blocked, password_digest, salt) VALUES (?,?,?,?,?,?,?,?,?)",
+        fields.fname,
+        fields.lname,
+        "/images/zerg.png",
+        fields.username,
+        fields.email,
+        false,
+        false,
+        encryption.digest(fields.password + salt),
+        salt
+      );
+    });
+    return res.redirect("/home");
+  });
+},
+
+  show: function(req, res){
+    db.get("SELECT * FROM users WHERE username = ?", req.query.username, function(err, user){
+      if(err) return console.error("Error retrieving data from table users.");
+      return res.render("users/show", {user: user});
+    });
+  },
+
+  update: function(req, res){
+    var form = formidable.IncomingForm();
+    form.parse(req, function(err, fields, files){
+      if(err) return console.error(err);
+      db.run("UPDATE users SET username=?, fname=?, lname=?, email=?, admin=?, blocked=?, password_digest=? WHERE id=?",
+        fields.username,
+        fields.fname,
+        fields.lname,
+        fields.email,
+        fields.admin,
+        fields.blocked,
+        fields.password,
+        function(err){
+          if(err) return console.err(err, "Error while updating table users.");
+      });
+    });
+  }
 };
 
 module.exports = exports = users;
